0f9462a4f23bf4a7381f5dafe73c4cd51bed2ba7
Jed Newcomb <newcomb.jed@gmail.com>
2024-10-13 10:56:44 -0400

    hello news

--- src/main/java/me/maktoba/LeetCode/AddTwoNumbers.java
+++ src/main/java/me/maktoba/LeetCode/AddTwoNumbers.java
@@ -22,14 +22,19 @@ public class AddTwoNumbers {
             //otherwise, its just the original digit (currSum)
             int last_digit = currSum % 10;
 
+            //make the new node and add it to the list
             ListNode new_node = new ListNode(last_digit);
             l3.next = new_node;
 
+            //move the iterators and the new list forward
+            //if possible
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
             l3 = l3.next;
         }
 
+        //if we have a carry of 1, we need to make it a new node
+        //to add to the list
         if (carry > 0) {
             ListNode new_node = new ListNode(carry);
             l3.next = new_node;
